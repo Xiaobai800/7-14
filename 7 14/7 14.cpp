b1a0b736018784ddd9edba1e127b9cdf7693a661
Xiaobai800 <2533760032@qq.com>
2022-07-14 22:37:36 +0800

    添加项目文件。

---
+++ 7 14/7 14.cpp
@@ -0,0 +1,64 @@
+#include<stdio.h>
+//int Fac1(int n)
+//{
+//	int i = 0;
+//	int ret = 1;
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret *= i;
+//	}
+//	return ret;
+//}
+//int Fac2(int n)
+//{
+//	if (n <= 1)
+//		return 1;
+//	else
+//		return n * Fac2(n - 1);
+//}
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	scanf_s("%d", &n);
+//	ret = Fac2(n);
+//	printf("%d\n", ret);
+//	return ;
+//}
+//int Fib(int n)
+//{
+//	if (n<= 2)
+//	      
+//		return 1;
+//	else
+//		return Fib(n - 1) + Fib(n - 2);
+//}
+//int Fib(int n)
+//{
+//	int a = 1;
+//	int b = 1;
+//	int c = 1;
+//	while (n > 2)
+//	{
+//		c = a + b;
+//		a = b;
+//		b = c;
+//		n--;
+//	}
+//	return c;
+//}
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	scanf_s("%d", &n);
+//	ret = Fib(n);
+//	printf("ret=%d\n", ret);
+//	return 0;
+//}
+#include<stdio.h>
+int main()
+{
+
+	return 0;
+}
